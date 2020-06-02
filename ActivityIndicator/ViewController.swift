0346837ef72af7e61279aaa5a517238c416c30a6
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2020-06-02 11:38:35 +0300

    init

--- ActivityIndicator/ViewController.swift
+++ ActivityIndicator/ViewController.swift
@@ -9,12 +9,27 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var activityIndicator: UIActivityIndicatorView = UIActivityIndicatorView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    @IBAction func buttonStart(_ sender: Any) {
+        
+        activityIndicator.center = self.view.center
+        activityIndicator.hidesWhenStopped = true
+        self.view.addSubview(activityIndicator)
+        
+        activityIndicator.startAnimating()
+    }
+    
+    @IBAction func buttonStop(_ sender: Any) {
+        
+        self.activityIndicator.stopAnimating()
+    }
 
 }
 
