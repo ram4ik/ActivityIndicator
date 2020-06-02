95039d7f269b365611adbca244fb8f4cb3835bd2
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2020-06-02 11:30:26 +0300

    Initial Commit

---
+++ ActivityIndicator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ActivityIndicator
+//
+//  Created by ramil on 02.06.2020.
+//  Copyright © 2020 com.ri. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
